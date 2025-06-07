0d7eef85be64410979b18321de082e14da82a698
Nelly Manukyan <nellymanukyan.a@gmail.com>
2025-06-07 15:39:07 +0400

    getting tasks tone

--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
 import dotenv from 'dotenv';
 dotenv.config();
-import fs from "fs";
+import fs from 'fs';
 const token: string | undefined = process.env.WRIKE_API_TOKEN;
 const url: string = 'https://www.wrike.com/api/v4/tasks?fields=[responsibleIds,parentIds]';
 
-interface mappedTask {
+interface IMappedTask {
     id: string;
     name: string;
     assignees: string[];
@@ -14,28 +14,23 @@ interface mappedTask {
     updated_at: string;
     ticket_url: string;
 }
-interface task {
+interface ITask {
     id: string,
-    accountId: string,
     title: string,
     parentIds: string[],
     responsibleIds: string[],
     status: string,
-    importance: string,
     createdDate: string,
     updatedDate: string,
-    dates: {
-    type: string
-},
-    scope: string,
-    customStatusId: string,
-    permalink: string,
-    priority: string
+    permalink: string
 }
+interface IGetResult {
+    kind: "tasks",
+    data : ITask[]
+}
+let mappedTasks: IMappedTask[] = [];
 
-let mappedTasks: mappedTask[] = [];
-
-async function getTasks(): Promise<void> {
+async function getTasks() {
     try {
         const response:Response = await fetch(url, {
             method: 'GET',
@@ -48,9 +43,9 @@ async function getTasks(): Promise<void> {
             throw new Error(`Request failed with status ${response.status}`);
         }
 
-        const getResult:{kind: "tasks", data : task[]} = await response.json();
+        const getResult:IGetResult = await response.json();
 
-        mappedTasks = getResult.data.map((value: task): mappedTask => ({
+        mappedTasks = getResult.data.map((value): IMappedTask => ({
             id: value.id,
             name: value.title,
             assignees: value.responsibleIds,
@@ -61,43 +56,22 @@ async function getTasks(): Promise<void> {
             ticket_url: value.permalink
         }));
 
-        await fs.promises.writeFile('tasks.json', JSON.stringify(mappedTasks, null, 2));
-        console.log('tasks.json has been saved!');
+        await writeTasksToFile('tasks.json', mappedTasks);
+        console.log(`Successfully added tasks`);
     } catch (e) {
         console.error('Fetch error:', e);
     }
 }
 
+function writeTasksToFile(filename: string, data: IMappedTask[]): Promise<void> {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(filename, JSON.stringify(data, null, 2), (err) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve();
+            }
+        });
+    })
+}
 getTasks();
-
-// fetch(url,
-//     {
-//         method: 'GET',
-//         headers: {
-//             'Authorization': `Bearer ${token}`
-//         }
-//     })
-//     .then(response  => response.json())
-//     .then((result:any):void=>{
-//         mappedTasks = result.data.map((value:any):task =>
-//              ({
-//                 id: value.id,
-//                 name: value.title,
-//                 assignees: value.responsibleIds,
-//                 status: value.status,
-//                 collections: value.parentIds,
-//                 created_at: value.createdDate,
-//                 updated_at: value.updatedDate,
-//                 ticket_url: value.permalink
-//             })
-//         );
-//        fs.writeFile('tasks.json', JSON.stringify(mappedTasks, null, 2), (err: NodeJS.ErrnoException | null) => {
-//            if (err) {console.log("omg error:",err);}
-//            else {
-//                console.log('great great!!');
-//            }
-//        })
-//     })
-//     .catch(err=>{
-//         console.log(err);
-//     });
